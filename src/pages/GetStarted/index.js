010306429bf13bd8abbf11e94450be75f5b5c557
irawanbudiarto <irawanbudiarto96@gmail.com>
2020-10-08 20:47:41 +0700

    Get Started

--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, ImageBackground} from 'react-native';
+import {ILGetStarted, ILLogo} from '../../assets';
+import {Button} from '../../components/atoms';
 
 const GetStarted = () => {
   return (
-    <View>
-      <Text>Get Started</Text>
-    </View>
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah & fleksibel
+        </Text>
+      </View>
+      <View>
+        <Button title="Get Started" />
+        <View style={{height: 16}} />
+        <Button title="Sign In" type="secondary" />
+      </View>
+    </ImageBackground>
   );
 };
 
 export default GetStarted;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '600',
+    color: 'white',
+    marginTop: 91,
+  },
+});
