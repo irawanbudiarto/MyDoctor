48f4f344f27cdb132e968fb4e8ceac7afc715457
irawanbudiarto <irawanbudiarto96@gmail.com>
2020-12-19 22:54:16 +0700

    navigate to register n login from get started

--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -3,7 +3,7 @@ import {Text, View, StyleSheet, ImageBackground} from 'react-native';
 import {ILGetStarted, ILLogo} from '../../assets';
 import {Button, Gap} from '../../components/atoms';
 
-const GetStarted = () => {
+const GetStarted = ({navigation}) => {
   return (
     <ImageBackground source={ILGetStarted} style={styles.page}>
       <View>
@@ -13,9 +13,20 @@ const GetStarted = () => {
         </Text>
       </View>
       <View>
-        <Button title="Get Started" />
+        <Button
+          title="Get Started"
+          onPress={() => {
+            navigation.navigate('Register');
+          }}
+        />
         <Gap height={16} />
-        <Button title="Sign In" type="secondary" />
+        <Button
+          title="Sign In"
+          type="secondary"
+          onPress={() => {
+            navigation.replace('Login');
+          }}
+        />
       </View>
     </ImageBackground>
   );
